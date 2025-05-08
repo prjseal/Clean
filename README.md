6459fe16eea20d8045559f8ffd4f4d5cc4c8f4d9
Paul Seal <prjseal@gmail.com>
2025-05-08 16:27:28 +0100

    got phil's headless stuff working for v15

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 If you want the older versions you need to go to the ones lower than version 4.0.0
 
 Latest Release Details:
+
 - Now works on Umbraco 15 with dotnet 9
 - You can now use the dotnet new template to install Clean Starter Kit with the Source Code rather than as a package
 
@@ -61,6 +62,44 @@ dotnet run --project "MyProject"
 #Running
 ```
 
+| :zap: If you're looking for Clean for Umbraco V9-12, see [Clean Starter Kit](https://github.com/prjseal/Clean-Starter-Kit-for-Umbraco-v9)! |
+| ------------------------------------------------------------------------------------------------------------------------------------------ |
 
-| :zap:        If you're looking for Clean for Umbraco V9-12, see [Clean Starter Kit](https://github.com/prjseal/Clean-Starter-Kit-for-Umbraco-v9)!   |
-|-----------------------------------------|
+## Headless Implementation
+
+Phil Whittaker has created a headless version of this starter kit available at [Clean Starter Kit Headless](https://github.com/hifi-phil/clean-headless)
+
+To set this up you will need to create an umbraco instance of the site as above
+
+Then turn on the content delivery API with the following change to the Umbraco property of the appsettings.json file
+
+```
+      "DeliveryApi": {
+        "Enabled": true
+      }
+```
+
+Finally to enable revalidation of content you will need to update the Enabled property as below (enmabled is currently set to false).
+
+```
+  "NextJs": {
+    "Revalidate": {
+      "Enabled": true,
+      "WebHookUrls": "[\"http://localhost:3000/api/revalidate\"]",
+      "WebHookSecret": "SOMETHING_SECRET"
+    }
+  }
+```
+
+This presumes that your healdess implementation will be loated at localhost:3000
+
+## New API endpoint
+
+We have added a new suite af API endpoints for bespoke functionality within the starter kit
+
+- Dictionary - access to the dictionary items
+- Search - the search form
+- Contact - the contact form submission
+
+We have added an OpenAPI instance available at
+/umbraco/swagger/index.html?urls.primaryName=Clean%20starter%20kit
---
+++ template/Clean.Headless/Controllers/DictionaryApiV1Controller.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Umbraco.Cms.Api.Common.Attributes;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace Clean.Headless.Controllers;
+
+[Route("api/v{version:apiVersion}/dictionary")]
+[ApiVersion("1.0")]
+[MapToApi("clean-starter")]
+[ApiExplorerSettings(GroupName = "Translation")]
+[ApiController]
+public class DictionaryApiV1Controller(
+	ILocalizationService localizationService,
+	ILogger<DictionaryApiV1Controller> logger)
+	: UmbracoApiController()
+{
+	
+	///<summary>
+	///Get all dictionary items
+	///</summary>
+	///<returns>All dictionary items</returns>
+	[HttpGet]
+	[Route("getdictionarytranslations")]
+	[ProducesResponseType(typeof(IEnumerable<TranslationModel>), StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+	public IActionResult GetDictionaryTranslations()
+	{
+		try
+		{
+			var culture = "en-US";
+			var dictionaryItems = localizationService.GetDictionaryItemDescendants(null);
+
+			var translationModels = dictionaryItems.Select(dictionaryItem => GetTranslationForDictionaryItem(dictionaryItem, culture));
+
+			return Ok(translationModels);
+		}
+		catch (Exception exception)
+		{
+			var message = "Caught error while getting translations for dictionary";
+			logger.LogError(exception, message);
+			return BadRequest(message);
+		}
+	}
+
+	/// <summary>
+	/// Gets the translation model for a dictionary item for the given culture
+	/// </summary>
+	/// <param name="dictionaryItem">dictionaryitem of which the translation is required</param>
+	/// <param name="culture">language to which is translated. format: ISO (nl-NL)</param>
+	/// <returns> translation model</returns>
+	private static TranslationModel GetTranslationForDictionaryItem(IDictionaryItem dictionaryItem, string culture)
+	{
+		return new TranslationModel
+		{
+			Id = dictionaryItem.Key,
+			Key = dictionaryItem.ItemKey,
+			Value = dictionaryItem.Translations.FirstOrDefault(translation => translation.LanguageIsoCode.Equals(culture, StringComparison.OrdinalIgnoreCase))?.Value ?? dictionaryItem.ItemKey
+		};
+	}
+
+	public class TranslationModel
+	{
+		public Guid? Id { get; init; }
+		public string? Key { get; init; }
+		public string? Value { get; init; }
+			
+	}
+}
