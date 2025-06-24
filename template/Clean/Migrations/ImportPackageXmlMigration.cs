3d1a5bed81864b7025f347ccb260377b0f3dceee
Warren Buckley <warren@hackmakedo.com>
2025-06-24 11:48:11 +0100

    Moves the publish of content into a MigrationPlansExecutedNotification
    Inspired by Umbraco SK
    https://github.com/umbraco/The-Starter-Kit/blob/v16/dev/src/Umbraco.SampleSite/Migrations/PostMigrationNotificationHandler.cs

--- template/Clean/Migrations/ImportPackageXmlMigration.cs
+++ template/Clean/Migrations/ImportPackageXmlMigration.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System.Linq;
+﻿using Microsoft.Extensions.Options;
 using System.Threading.Tasks;
 using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.IO;
-using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.PropertyEditors;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Strings;
@@ -15,9 +12,6 @@ namespace Clean.Migrations
 {
     public class ImportPackageXmlMigration : AsyncPackageMigrationBase
     {
-        private readonly IContentService _contentService;
-        private readonly ILogger<ImportPackageXmlMigration> _logger;
-
         public ImportPackageXmlMigration(
             IPackagingService packagingService,
             IMediaService mediaService,
@@ -26,9 +20,7 @@ namespace Clean.Migrations
             IShortStringHelper shortStringHelper,
             IContentTypeBaseServiceProvider contentTypeBaseServiceProvider,
             IMigrationContext context,
-            IOptions<PackageMigrationSettings> packageMigrationSettings,
-            IContentService contentService,
-            ILogger<ImportPackageXmlMigration> logger)
+            IOptions<PackageMigrationSettings> packageMigrationSettings)
             : base(packagingService,
                 mediaService,
                 mediaFileManager,
@@ -37,28 +29,12 @@ namespace Clean.Migrations
                 contentTypeBaseServiceProvider,
                 context, packageMigrationSettings)
         {
-            _contentService = contentService;
-            _logger = logger;
         }
 
         protected async override Task MigrateAsync()
         {
             //import the xml package
             ImportPackage.FromEmbeddedResource(GetType()).Do();
-
-            //publish the home page with all descendants
-            await Task.Run(() =>
-            {
-                var contentHome = _contentService.GetRootContent().FirstOrDefault(x => x.ContentType.Alias == "home");
-                if (contentHome != null)
-                {
-                    _contentService.PublishBranch(contentHome, PublishBranchFilter.All, new[] { "en-US" });
-                }
-                else
-                {
-                    _logger.LogWarning("The installed Home page was not found");
-                }
-            });
         }
     }
 }
---
+++ template/Clean/Migrations/PostMigrationNotification.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Infrastructure.Migrations;
+using Umbraco.Cms.Infrastructure.Migrations.Notifications;
+
+namespace Clean.Migrations
+{
+    public class PostMigrationNotificationHandler : INotificationAsyncHandler<MigrationPlansExecutedNotification>
+    {
+        private readonly IContentService _contentService;
+        private readonly ILogger<PostMigrationNotificationHandler> _logger;
+
+        public PostMigrationNotificationHandler(
+            IContentService contentService,
+            ILogger<PostMigrationNotificationHandler> logger)
+        {
+            _contentService = contentService;
+            _logger = logger;
+        }
+
+        public Task HandleAsync(MigrationPlansExecutedNotification notification, CancellationToken cancellationToken)
+        {
+            // Check if we have run the right migration, otherwise skip  
+            if (HasMigrationRun(notification.ExecutedPlans) is false)
+            {
+                return Task.CompletedTask;
+            }
+
+            var contentHome = _contentService.GetRootContent().FirstOrDefault(x => x.ContentType.Alias == "home");
+            if (contentHome != null)
+            {
+                var publishResult = _contentService.PublishBranch(contentHome, PublishBranchFilter.All, []);
+                foreach (var item in publishResult)
+                {
+                    _logger.LogInformation("Result of publishing '{nodeName}' = {publishResult}", item.Content.Name, item.Result.ToString());
+                }
+            }
+            else
+            {
+                _logger.LogWarning("The installed Home page was not found");
+            }
+            return Task.CompletedTask;
+        }
+
+        private bool HasMigrationRun(IEnumerable<ExecutedMigrationPlan> executedMigrationPlans)
+        {
+            foreach (ExecutedMigrationPlan executedMigrationPlan in executedMigrationPlans)
+            {
+                foreach (MigrationPlan.Transition transition in executedMigrationPlan.CompletedTransitions)
+                {
+                    if (transition.MigrationType == typeof(ImportPackageXmlMigration))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
